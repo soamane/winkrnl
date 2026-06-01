0a3d05d0f18719b18ddad2f140d72e68587722e0
soamane <soamane@mail.ru>
2026-06-01 18:32:45 +0300

    PE utils impl

---
+++ winkrnl/src/utils/pe_utils.cpp
@@ -0,0 +1,64 @@
+﻿#include "pe_utils.hpp"
+
+#include <print>
+
+PIMAGE_NT_HEADERS Utils::PE::GetNtHeaders(void* image)
+{
+    if (!image) {
+        std::println("[-] Image is nullptr");
+        return nullptr;
+    }
+
+    PIMAGE_DOS_HEADER dosHeader = reinterpret_cast<PIMAGE_DOS_HEADER>(image);
+    if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE) {
+        std::println("[-] Invalid DOS signature");
+        return nullptr;
+    }
+
+    PIMAGE_NT_HEADERS ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<uintptr_t>(image) + dosHeader->e_lfanew);
+    if (ntHeaders->Signature != IMAGE_NT_SIGNATURE) {
+        std::println("[-] Invalid NT signature");
+        return nullptr;
+    }
+
+    return ntHeaders;
+}
+
+std::vector<Utils::PE::Import> Utils::PE::GetImports(void* image, PIMAGE_NT_HEADERS ntHeaders)
+{
+    const auto pImage = reinterpret_cast<uintptr_t>(image);
+
+    const auto importsDirAddr = ntHeaders->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress;
+    auto importsDescriptor = reinterpret_cast<PIMAGE_IMPORT_DESCRIPTOR>(pImage + importsDirAddr);
+
+    std::vector<Import> imports;
+    while (importsDescriptor->FirstThunk) {
+        Import i;
+        i.name = std::string(reinterpret_cast<char*>(pImage + importsDescriptor->Name));
+
+        std::println("[+] Import: {}", i.name);
+
+        auto firstThunk = reinterpret_cast<PIMAGE_THUNK_DATA>(pImage + importsDescriptor->FirstThunk);
+        auto originFirstThunk = reinterpret_cast<PIMAGE_THUNK_DATA>(pImage + importsDescriptor->OriginalFirstThunk);
+
+        while (originFirstThunk->u1.Function) {
+            const auto thunkInfo = reinterpret_cast<PIMAGE_IMPORT_BY_NAME>(pImage + originFirstThunk->u1.AddressOfData);
+
+            Import::Thunk thunk;
+            thunk.name = thunkInfo->Name;
+            thunk.address = firstThunk->u1.Function;
+
+            i.thunks.push_back(thunk);
+
+            std::println("  > Function: {} Address: 0x{:X}", thunk.name, thunk.address);
+
+            ++firstThunk;
+            ++originFirstThunk;
+        }
+
+        imports.push_back(i);
+        ++importsDescriptor;
+    }
+
+    return imports;
+}
---
+++ winkrnl/src/utils/pe_utils.hpp
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#ifndef PE_UTILS_HPP
+#define PE_UTILS_HPP
+
+#include <string>
+#include <vector>
+#include <windows.h>
+
+namespace Utils::PE {
+struct Import {
+    struct Thunk {
+        std::string name;
+        uintptr_t address;
+    };
+
+    std::string name;
+    std::vector<Thunk> thunks;
+};
+
+PIMAGE_NT_HEADERS GetNtHeaders(void* image);
+std::vector<Import> GetImports(void* image, PIMAGE_NT_HEADERS ntHeaders);
+}
+
+#endif // !PE_UTILS_HPP
