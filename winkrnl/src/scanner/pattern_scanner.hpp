48579308ceefd940b317f250ebb281cb287ab51e
soamane <soamane@mail.ru>
2026-06-05 01:01:20 +0300

    Pattern scanner impl

---
+++ winkrnl/src/scanner/pattern_scanner.hpp
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#ifndef PATTERN_SCANNER_HPP
+#define PATTERN_SCANNER_HPP
+
+#include <memory>
+#include <cstdint>
+
+class BasicVulnDriver;
+
+class PatternScanner {
+public:
+    explicit PatternScanner(std::shared_ptr<BasicVulnDriver> driver);
+    ~PatternScanner();
+
+public:
+    uintptr_t FindPatternAddr(uintptr_t moduleBase, size_t moduleSize, const char* pattern, const char* mask) const;
+
+private:
+    std::shared_ptr<BasicVulnDriver> driver;
+};
+
+#endif // !PATTERN_SCANNER_HPP
