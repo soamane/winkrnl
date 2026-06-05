b4e941e68d9496cb72991686ee3e6c55661ee589
soamane <soamane@mail.ru>
2026-06-05 17:17:48 +0300

    Update

--- winkrnl/src/main.cpp
+++ winkrnl/src/main.cpp
@@ -5,15 +5,18 @@
 #include <vuln/vuln_driver_loader.hpp>
 
 #include <kernel/k32_context.hpp>
+#include <kernel/k32_parser.hpp>
 
 #include <mapper/driver_mapper.hpp>
+
+#include <vuln/vuln_trace_cleaner.hpp>
 
 #include <filesystem>
 #include <print>
 
 int main(int argc, char** argv)
 {
-    if (argc != 2) {
+    /*if (argc != 2) {
         std::println("Usage: {} <driver_path>", std::filesystem::path(argv[0]).filename().string());
         return EXIT_FAILURE;
     }
@@ -28,7 +31,7 @@ int main(int argc, char** argv)
     if (driverBytes.empty()) {
         std::println("[-] Failed to read driver file");
         return EXIT_FAILURE;
-    }
+    }*/
 
     if (!VulnDriverLoader::EnableLoadPrivileges()) {
         std::println("[-] Failed to allow load privileges");
@@ -44,12 +47,17 @@ int main(int argc, char** argv)
     }
 
     auto k32ctx = std::make_shared<K32Context>(iqvw64);
-
-    DriverMapper driverMapper = DriverMapper(k32ctx);
+    auto vulnTraceCleaner = VulnTraceCleaner(k32ctx);
+    if (!vulnTraceCleaner.Cleanup()) {
+        std::println("[-] Failed to cleanup vulnerable driver traces");
+        return EXIT_FAILURE;
+    }
+   
+    /*DriverMapper driverMapper = DriverMapper(k32ctx);
     if (!driverMapper.Map((void*)driverBytes.data())) {
         std::println("[-] Failed to mmap driver");
         return EXIT_FAILURE;
-    }
+    }*/
 
     if (!driverLoader.Unload()) {
         std::println("[-] Failed to unload vulnerable driver");
